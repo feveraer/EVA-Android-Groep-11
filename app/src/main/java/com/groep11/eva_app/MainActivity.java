825e73ff4e674b27433f0d1adac870e0a2d82cf6
Tjen Wellens <tjen.wellens@gmail.com>
2015-10-14 20:29:17 +0200

    fixed no-actionbar bug

--- app/src/main/java/com/groep11/eva_app/MainActivity.java
+++ app/src/main/java/com/groep11/eva_app/MainActivity.java
@@ -1,11 +1,11 @@
 package com.groep11.eva_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
