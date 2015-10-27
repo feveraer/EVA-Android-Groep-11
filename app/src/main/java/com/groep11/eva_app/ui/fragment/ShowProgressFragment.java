0dfb2c270cf84bbe522fc11aa04f10f1b2bfa76a
Brian Pinsard <brian.pinsard.u8248@student.hogent.be>
2015-10-27 12:18:01 +0100

    - Apply the dummy animation to the progress ImageView onClick

--- app/src/main/java/com/groep11/eva_app/ui/fragment/ShowProgressFragment.java
+++ app/src/main/java/com/groep11/eva_app/ui/fragment/ShowProgressFragment.java
@@ -2,15 +2,14 @@ package com.groep11.eva_app.ui.fragment;
 
 
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.groep11.eva_app.R;
 
@@ -52,9 +51,17 @@ public class ShowProgressFragment extends Fragment {
 
     public void increaseProgress(){
         if(progressCounter < 21) progressCounter++;
-        //TODO: animation, this is a simple test :)
-        progressImage.setBackgroundColor(Color.rgb(125, progressCounter * 12, 0));
+
+        progressImage.setBackgroundColor(Color.rgb(125, progressCounter * 12, 0));                  //TODO: remove this after animations are implemented
         Log.i(TAG, "progress..." + progressCounter);
+
+        //Swap between two progress animationResources (even/uneven)
+        //TODO: use real animation fases
+        progressImage.setBackgroundResource((progressCounter % 2 == 0) ? R.drawable.dummy_progress_fase_1 : R.drawable.dummy_progress_fase_2);
+
+        //Get the drawable animation and start it
+        AnimationDrawable progressAnimation = (AnimationDrawable) progressImage.getBackground();
+        progressAnimation.start();
     }
 
     @OnClick(R.id.fragment_show_progress_container)
