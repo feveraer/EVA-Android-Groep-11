1fbfde882d0cb51724a5e68d24592b2fcae1fa74
feveraer <frederic.everaert@gmail.com>
2015-10-31 00:31:26 +0100

    - implemented LoaderCallbacks - info log all current categories

--- app/src/main/java/com/groep11/eva_app/ui/fragment/CategoryFragment.java
+++ app/src/main/java/com/groep11/eva_app/ui/fragment/CategoryFragment.java
@@ -1,16 +1,38 @@
 package com.groep11.eva_app.ui.fragment;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.groep11.eva_app.R;
+import com.groep11.eva_app.data.EvaContract;
 
-public class CategoryFragment extends Fragment {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CategoryFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final String TAG = "CATEGORY";
+
+    private Uri mUri;
+
+    private static final int CATEGORY_LOADER = 0;
+
+    @Bind(R.id.category_button_one)
+    Button mButtonOne;
+    @Bind(R.id.category_button_two)
+    Button mButtonTwo;
+    @Bind(R.id.category_button_three)
+    Button mButtonThree;
 
     public static CategoryFragment newInstance() {
         return new CategoryFragment();
@@ -28,7 +50,54 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mUri = EvaContract.ChallengeEntry.buildCurrentCategoriesUri();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_category, container, false);
+        // Non-activity binding for butterknife
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(CATEGORY_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (null != mUri) {
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    mUri,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data != null && data.moveToFirst()) {
+            // Log every row
+            int i = 0;
+            while (data.moveToNext()) {
+                Log.i(TAG, data.getString(0));
+                i++;
+            }
+            Log.i(TAG, String.format("%d", i));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
