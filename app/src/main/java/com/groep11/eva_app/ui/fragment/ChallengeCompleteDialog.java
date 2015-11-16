05f6403b13fe04dbb9de2449a6097f9e3eb20c37
feveraer <frederic.everaert@gmail.com>
2015-11-16 03:09:08 +0100

    - ok button for dialog

--- app/src/main/java/com/groep11/eva_app/ui/fragment/ChallengeCompleteDialog.java
+++ app/src/main/java/com/groep11/eva_app/ui/fragment/ChallengeCompleteDialog.java
@@ -3,6 +3,7 @@ package com.groep11.eva_app.ui.fragment;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import android.view.ViewGroup;
 import com.groep11.eva_app.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class ChallengeCompleteDialog extends DialogFragment {
@@ -28,7 +31,12 @@ public class ChallengeCompleteDialog extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_challenge_complete, container);
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_challenge_complete, container, false);
+        // Non-activity binding for butterknife
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
     }
 
     @Override
@@ -37,5 +45,10 @@ public class ChallengeCompleteDialog extends DialogFragment {
         // Fetch arguments from bundle and set title (+ default value)
         String title = getArguments().getString("title", "Congratulations!");
         getDialog().setTitle(title);
+    }
+
+    @OnClick(R.id.dialog_btn_ok)
+    public void onOk(View view) {
+        this.dismiss();
     }
 }
