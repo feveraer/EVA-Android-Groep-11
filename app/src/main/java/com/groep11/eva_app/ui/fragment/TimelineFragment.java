933624eb55d5819fb6baa4bb50d27b51a6188b75
Brian Pinsard <brian.pinsard.u8248@student.hogent.be>
2015-11-21 17:34:12 +0100

    Created ListFragment for timeline

---
+++ app/src/main/java/com/groep11/eva_app/ui/fragment/TimelineFragment.java
@@ -0,0 +1,77 @@
+package com.groep11.eva_app.ui.fragment;
+
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.groep11.eva_app.R;
+import com.groep11.eva_app.data.EvaContract;
+import com.groep11.eva_app.ui.fragment.interfaces.IColumnConstants;
+import com.groep11.eva_app.util.ChallengeEntryAdapter;
+
+public class TimelineFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>, IColumnConstants {
+    public static final String TAG = "TIMELINE";
+    private static final int LOADER = 99;
+
+    private ChallengeEntryAdapter entryAdapter;
+
+    public TimelineFragment() {
+        // Required empty public constructor
+    }
+
+    public static TimelineFragment newInstance() {
+        TimelineFragment fragment = new TimelineFragment();
+        return  fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_timeline, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getLoaderManager().initLoader(LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.d(TAG, "onCreateLoader() called with: " + "id = [" + id + "], args = [" + args + "]");
+        CursorLoader loader = new CursorLoader(
+                this.getActivity(),
+                EvaContract.ChallengeEntry.buildChallengesTodayURI(), //TODO: only completed challenges URI
+                TABLE_COLUMNS,
+                null,
+                null,
+                null
+        );
+
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, "onLoadFinished() called with: " + "loader = [" + loader + "], data = [" + data + "]");
+        Log.d(TAG, "onLoadFinished: data amount = " + data.getCount());
+        entryAdapter = new ChallengeEntryAdapter(getActivity(), data, 0);
+        setListAdapter(entryAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
