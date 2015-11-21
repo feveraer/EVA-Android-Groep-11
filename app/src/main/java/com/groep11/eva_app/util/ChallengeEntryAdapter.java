6f043fd88705fdcc079f9e75e1a976f308e99298
Brian Pinsard <brian.pinsard.u8248@student.hogent.be>
2015-11-21 17:34:57 +0100

    - Difficulty leaves opacity now working correctly - Added Lines for the timeline entries

--- app/src/main/java/com/groep11/eva_app/util/ChallengeEntryAdapter.java
+++ app/src/main/java/com/groep11/eva_app/util/ChallengeEntryAdapter.java
@@ -2,6 +2,7 @@ package com.groep11.eva_app.util;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,29 +37,41 @@ public class ChallengeEntryAdapter extends CursorAdapter implements IColumnConst
 
         ViewHolder viewHolder = new ViewHolder(view);
         view.setTag(viewHolder);
-
         return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        Log.d(TAG, "bindView() called with: " + "view = [" + view + "], context = [" + context + "], cursor = [" + cursor + "]");
         ViewHolder holder = (ViewHolder) view.getTag();
 
         // Get values from cursor
         String title = cursor.getString(COL_CHALLENGE_TITLE);
-        int diff = cursor.getInt(COL_CHALLENGE_ID);
+        int diff = cursor.getInt(COL_CHALLENGE_DIFFICULTY);
+        Log.d(TAG, "bindView: " + diff);
 
+        // Set challenge title
         holder.entryTitle.setText(title);
 
         // Set opacity leaf #3
         holder.entryLeaves.get(2).setAlpha(diff < 3 ? LEAF_DISABLED_OPACITY : 1);
         // Set opacity leaf #2
         holder.entryLeaves.get(1).setAlpha(diff < 2 ? LEAF_DISABLED_OPACITY : 1);
+
+        // Entry is the latest challenge, don't draw a top line (no challenge above it)
+        if(cursor.getPosition() != 0)
+            holder.entryLineTop.setBackgroundColor(Color.parseColor("#696b71"));
+
+        // Entry is the last challenge, don't draw bottom line (no challenge below it)
+        if(cursor.getPosition() != cursor.getCount()-1)
+            holder.entryLineBottom.setBackgroundColor(Color.parseColor("#696b71"));
     }
 
     static class ViewHolder {
         @Bind(R.id.timeline_enty_title) TextView entryTitle;
-        @Bind(R.id.timeline_entry_line) ImageView entryLine;
+        @Bind(R.id.timeline_entry_icon) ImageView entryCategoryIcon;
+        @Bind(R.id.timeline_entry_line_top) ImageView entryLineTop;
+        @Bind(R.id.timeline_entry_line_bottom) ImageView entryLineBottom;
         @Bind({R.id.timeline_entry_leaf_1, R.id.timeline_entry_leaf_2, R.id.timeline_entry_leaf_3}) List<ImageView> entryLeaves;
 
         public ViewHolder(View view) {
