aa02b3cf18dfebdf5adc12b94b5fb494c4c65756
Brian Pinsard <brian.pinsard.u8248@student.hogent.be>
2015-12-03 14:53:25 +0100

    Replaced the video with a static blurred frame from the video

--- app/src/main/java/com/groep11/eva_app/ui/activity/RegistrationActivity.java
+++ app/src/main/java/com/groep11/eva_app/ui/activity/RegistrationActivity.java
@@ -1,37 +1,21 @@
 package com.groep11.eva_app.ui.activity;
 
-import android.accounts.AccountManager;
 import android.app.FragmentTransaction;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.widget.MediaController;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.widget.VideoView;
 
 import com.groep11.eva_app.R;
 import com.groep11.eva_app.ui.fragment.LoginFragment;
 import com.groep11.eva_app.ui.fragment.CategoryFragment;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 public class RegistrationActivity extends TransparentBarActivity {
     public static final String ARG_ACCOUNT_TYPE = "argAuthType";
     public static final String ARG_AUTH_TYPE = "argAuthType";
     public static final String ARG_IS_ADDING_NEW_ACCOUNT = "argIsAddingNewAccount";
 
-    @Bind(R.id.video_registration)
-    VideoView mVideo;
-
-    private MediaController mediaController;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
-        ButterKnife.bind(this);
 
         // Make our action bar transparent for future use
         initActionBar();
@@ -48,26 +32,5 @@ public class RegistrationActivity extends TransparentBarActivity {
         fragmentTransaction.commit();
 
         setOnBackStackChangedListener();
-
-        startBackgroundVideo();
-    }
-
-    private void startBackgroundVideo(){
-        // Get our video's path set it as the VideoView's resource
-        int videoId = getResources().getIdentifier("vegimovie", "raw", getPackageName());
-        String videoPath = "android.resource://" + getPackageName() + "/" + videoId;
-
-        mVideo.setVideoURI(Uri.parse(videoPath));
-
-        // Make the video loop
-        mVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
-            }
-        });
-
-        // Start the video
-        mVideo.start();
     }
 }
